6cab8ee9bb57fbb9c98049482009f17ca4586968
yanegoat <163878691+yanegoat@users.noreply.github.com>
2024-03-18 20:11:28 +0200

    Task 1,2

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import psycopg2
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def create_rw_conn():
+    # pass along secrets to pyscopg2
+    PORT = "5432"
+    USER = "postgres"
+    PASSWORD = "admin"
+    DATABASE = "contact_book"
+    HOST = "localhost"
 
-# Press the green button in the gutter to run the script.
+    # create connection string
+    conn = psycopg2.connect(host=HOST, port=PORT, user=USER,
+                            database=DATABASE, password=PASSWORD)
+    print("Connection established")
+
+    return conn
+
+
+def init_table():
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        email TEXT,
+                        phone TEXT)''')
+    print("Table created successfully")
+    cursor.close()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    conn = create_rw_conn()
+    init_table()
+    conn.commit()
+    conn.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
